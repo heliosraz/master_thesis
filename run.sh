f6a03c9cb97b78f65d3e0a9a3b0b3b1306cc90b7
heliosraz <heliosra3721@gmail.com>
2025-03-25 22:35:12 -0400

    modified run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#SBATCH --job-name=data
+#SBATCH --job-name=task1_thes
 #SBATCH --output=output.txt
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=40964
 
 source ~/miniconda3/bin/activate nlp
-python3 
+python3 ./experiments/exp1.py 0 1
