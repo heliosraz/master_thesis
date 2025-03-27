1bf2303b886af99787e3ff00c59478ff860911c2
heliosraz <heliosra3721@gmail.com>
2025-03-27 14:11:52 -0400

    batch script parition information added

--- data.sh
+++ data.sh
@@ -4,6 +4,7 @@
 #SBATCH --output=data_output.txt
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=40964
+#SBATCH --partition=regular
 
 source ~/miniconda3/bin/activate nlp
 PYTHONPATH=$PWD python3 ./processing/generate_task.py
--- experiments.sh
+++ experiments.sh
@@ -5,6 +5,7 @@
 #SBATCH --gres=gpu:4
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=40964
+#SBATCH --partition=regular
 
 source ~/miniconda3/bin/activate nlp
 PYTHONPATH=$PWD python3 ./experiments/exp1.py
