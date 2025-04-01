4ec42020205e0cfa54feb079939af12c6d34d075
heliosraz <heliosra3721@gmail.com>
2025-04-01 11:20:04 -0400

    unbatched iterative prompts

--- models/gemma.py
+++ models/gemma.py
@@ -1,3 +1,4 @@
+from typing import List
 from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline, QuantoConfig
 from torch import nn
 
@@ -9,8 +10,15 @@ class Gemma(nn.Module):
         self.model = AutoModelForCausalLM.from_pretrained(model_id, quantization_config = quant_config, device_map=device, trust_remote_code=True)
         self.device = device
         
-    def forward(self, prompt: str):
-        input_prompt = self.tokenizer(prompt, return_tensors="pt")
-        outputs = self.model.generate(input_prompt.input_ids.to(self.device), num_beams = 3, max_new_tokens = 100, attention_mask=input_prompt.attention_mask.to(self.device))
-        response = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0][:len(prompt)]
-        return response
+    def forward(self, prompts: List[str]):
+        messages = []
+        prompt = ""
+        for p in prompts["prompt"]:
+            messages.append({"role": "user", "content": p})
+            prompt += p + "\n"
+            input_prompt = self.tokenizer(prompt, return_tensors="pt")
+            outputs = self.model.generate(input_prompt.input_ids.to(self.device), num_beams = 5, max_new_tokens = 100, attention_mask=input_prompt.attention_mask.to(self.device))
+            response = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)[0][:len(prompt)]
+            prompt += "Answer: "+response +"\n"
+            messages.append({"role": "assistant", "content": response})
+        return messages
