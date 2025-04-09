84febf976ce0d25678a1bf8896527b37d2661ce9
heliosraz <heliosra3721@gmail.com>
2025-04-09 18:46:01 -0400

    debug

--- shell/llama.sh
+++ shell/llama.sh
@@ -2,9 +2,11 @@
 
 #SBATCH --job-name=llama
 #SBATCH --output=./output/llama_output.txt
-#SBATCH --error=./output/llama_error.txt
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=40964
 #SBATCH --partition=regular
+#SBATCH --debug
 
 source ~/miniconda3/bin/activate nlp
 echo "TESTING"
