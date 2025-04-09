69bebb3c5acf148410c97753e94a1d715958ac3d
heliosraz <heliosra3721@gmail.com>
2025-04-09 17:58:21 -0400

    debug

--- shell/llama.sh
+++ shell/llama.sh
@@ -3,8 +3,6 @@
 #SBATCH --job-name=llama
 #SBATCH --output=./output/llama_output.txt
 #SBATCH --gres=gpu:4
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=40964
 #SBATCH --partition=regular
 
 source ~/miniconda3/bin/activate nlp
