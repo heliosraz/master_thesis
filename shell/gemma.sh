027b1697289620134b2b87477c828efdb822e544
heliosraz <heliosra3721@gmail.com>
2025-04-10 00:18:13 -0400

    debug scripts (changing gemma)

--- shell/gemma.sh
+++ shell/gemma.sh
@@ -6,6 +6,12 @@
 #SBATCH --cpus-per-task=10
 #SBATCH --mem-per-cpu=40964
 #SBATCH --partition=regular
+#SBATCH --error=./output/mistral_error.txt
+
+echo "Starting the script..."
+echo "SLURM_JOB_ID: $SLURM_JOB_ID"
+echo "SLURM_NODELIST: $SLURM_NODELIST"
+echo "SLURM_JOB_NAME: $SLURM_JOB_NAME"
 
 source ~/miniconda3/bin/activate nlp
 if [ -n "$1" ]; then
