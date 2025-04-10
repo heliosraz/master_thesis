76787060ab264408f01d211e51fe73154d7ea9b7
heliosraz <heliosra3721@gmail.com>
2025-04-10 02:14:50 -0400

    forgot to change back to cuda

--- models/llama.py
+++ models/llama.py
@@ -4,7 +4,7 @@ from torch import nn
 import torch
 
 class Llama(nn.Module):
-    def __init__(self, model_id: str = "meta-llama/Llama-3.2-3B-Instruct", device: str = "mps"):
+    def __init__(self, model_id: str = "meta-llama/Llama-3.2-3B-Instruct", device: str = "cuda"):
         super(Llama, self).__init__()
         quant_config = QuantoConfig(weights="int4")
         self.tokenizer = AutoTokenizer.from_pretrained(model_id)
