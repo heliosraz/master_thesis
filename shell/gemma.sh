ec05ba7746aafb543163e70f7bf682ee842f9e45
heliosraz <heliosra3721@gmail.com>
2025-04-10 00:32:10 -0400

    testing shell(gemma)

--- shell/gemma.sh
+++ shell/gemma.sh
@@ -1,22 +1,20 @@
 #!/bin/bash
-
-#SBATCH --job-name=gemma
-#SBATCH --error=gemma_error.txt
-#SBATCH --output=./output/gemma_output.txt
-#SBATCH --ntasks=10
-#SBATCH --mem-per-cpu=40964
-#SBATCH --partition=regular
-
+#SBATCH --job-name=gemma_test
+#SBATCH --output=test_output.txt
+#SBATCH --error=test_error.txt
+#SBATCH --cpus-per-task=1
 
 echo "Starting the script..."
 echo "SLURM_JOB_ID: $SLURM_JOB_ID"
 echo "SLURM_NODELIST: $SLURM_NODELIST"
 echo "SLURM_JOB_NAME: $SLURM_JOB_NAME"
+sleep 20
 
 source ~/miniconda3/bin/activate nlp
-if [ -n "$1" ]; then
-    PYTHONPATH=~/master_thesis python3 ~/master_thesis/experiments/exp1.py 1 $1
-else
-    PYTHONPATH=~/master_thesis python3 ~/master_thesis/experiments/exp1.py 1
-fi
+echo "Activated conda environment 'nlp'."
+# if [ -n "$1" ]; then
+#     PYTHONPATH=~/master_thesis python3 ~/master_thesis/experiments/exp1.py 1 $1
+# else
+#     PYTHONPATH=~/master_thesis python3 ~/master_thesis/experiments/exp1.py 1
+# fi
 
