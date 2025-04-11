d7ace5233d54dbf3aa2c27ecd9cb27305e5dd3f9
heliosraz <heliosra3721@gmail.com>
2025-04-11 14:18:38 -0400

    decreasing shell resources

--- shell/llama.sh
+++ shell/llama.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:4
-#SBATCH --cpus-per-task=10
-#SBATCH --mem-per-cpu=40964
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=4 
+#SBATCH --mem-per-cpu=16384
 #SBATCH --partition=regular
 
 source ~/miniconda3/bin/activate nlp
