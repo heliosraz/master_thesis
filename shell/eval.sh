06566b9bda1c3887262c8ed3ee0045e6c0118afd
heliosraz <heliosra3721@gmail.com>
2025-04-15 17:56:59 -0400

    adjusting batch size

--- shell/eval.sh
+++ shell/eval.sh
@@ -3,7 +3,6 @@
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4 
 #SBATCH --mem-per-cpu=16384
-#SBATCH --partition=gpu48g
 
 source ~/miniconda3/bin/activate thesis
 if [ -n "$1" ] && [ -n "$2" ]; then
@@ -13,3 +12,23 @@ elif [ -n "$1" ]; then
 else
     python ~/master_thesis/experiments/eval.py
 fi
+
+cat shell/output/eval_1_llama_output.txt
+cat shell/output/eval_1_gemma_output.txt
+cat shell/output/eval_1_mistral_output.txt
+cat shell/output/eval_1_dpseek_output.txt
+
+cat shell/output/eval_2_llama_output.txt
+cat shell/output/eval_2_gemma_output.txt
+cat shell/output/eval_2_mistral_output.txt
+cat shell/output/eval_2_dpseek_output.txt
+
+cat shell/output/eval_3_llama_output.txt
+cat shell/output/eval_3_gemma_output.txt
+cat shell/output/eval_3_mistral_output.txt
+cat shell/output/eval_3_dpseek_output.txt
+
+cat shell/output/eval_4_llama_output.txt
+cat shell/output/eval_4_gemma_output.txt
+cat shell/output/eval_4_mistral_output.txt
+cat shell/output/eval_4_dpseek_output.txt
