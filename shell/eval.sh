3e4c885edb45f190dac01951f0f3a485eaffceb1
heliosraz <heliosra3721@gmail.com>
2025-04-15 00:43:17 -0400

    bug fixes

--- shell/eval.sh
+++ shell/eval.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-#SBATCH --job-name=eval
-#SBATCH --output=./shell/output/eval_output.txt
 #SBATCH --gres=gpu:1
 #SBATCH --cpus-per-task=4 
 #SBATCH --mem-per-cpu=16384
--- shell/mass_eval.sh
+++ shell/mass_eval.sh
@@ -5,5 +5,5 @@
 
 sbatch --job-name=eval_llama --output=./shell/output/eval_llama_output.txt shell/eval.sh 0
 sbatch --job-name=eval_gemma --output=./shell/output/eval_gemma_output.txt shell/eval.sh 1
-sbatch --job-name=eval_mistral --output=./shell/output/eval_llama_output.txt shell/eval.sh 2
+sbatch --job-name=eval_mistral --output=./shell/output/eval_mistral_output.txt shell/eval.sh 2
 sbatch --job-name=eval_dpseek --output=./shell/output/eval_dpseek_output.txt shell/eval.sh 3
