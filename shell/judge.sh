48772e0c7aa59b5365c3d911f1b594d563a93be7
heliosraz <heliosra3721@gmail.com>
2025-04-18 23:58:39 -0400

    forgot to change rename is shell scripts; standardize naming

--- shell/judge.sh
+++ shell/judge.sh
@@ -6,9 +6,9 @@
 
 source ~/miniconda3/bin/activate thesis
 if [ -n "$1" ] && [ -n "$2" ]; then
-    python ~/master_thesis/experiments/eval.py $1 $2
+    python ~/master_thesis/experiments/judge.py $1 $2
 elif [ -n "$1" ]; then
-    python ~/master_thesis/experiments/eval.py $1
+    python ~/master_thesis/experiments/judge.py $1
 else
-    python ~/master_thesis/experiments/eval.py
+    python ~/master_thesis/experiments/judge.py
 fi
