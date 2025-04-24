18e3ac5ad5df7eaaa0fa649b358f91c1896f87fd
heliosraz <heliosra3721@gmail.com>
2025-04-24 15:49:25 -0400

    shell script output file DNE

--- shell/compress.sh
+++ shell/compress.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #SBATCH --job-name=compress
-#SBATCH --output=/output/compress_output.txt
+#SBATCH --output=./shell/output/compress_output.txt
 #SBATCH --cpus-per-task=4 
 #SBATCH --mem-per-cpu=16384
 #SBATCH --partition=regular
