8eb83ec686650b3e143ed306a3af60445628bf7e
heliosraz <heliosra3721@gmail.com>
2025-04-24 15:48:11 -0400

    compression script

---
+++ shell/compress.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --job-name=compress
+#SBATCH --output=/output/compress_output.txt
+#SBATCH --cpus-per-task=4 
+#SBATCH --mem-per-cpu=16384
+#SBATCH --partition=regular
+
+cd ~/master_thesis
+tar -czvf embed_results.tar.gz results/embed/
