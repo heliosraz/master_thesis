99bc44fde34d416e601bde3623482a1afea95e64
heliosraz <heliosra3721@gmail.com>
2025-12-21 09:58:53 -0800

    testing out whether removing structured output backend fixes bad request error

--- shell/models/llama.sh
+++ shell/models/llama.sh
@@ -1,6 +1,5 @@
 vllm serve meta-llama/Llama-3.2-3B-Instruct --max_num_batched_tokens 7376 \
                         --max_model_len 7376 \
                         --enforce-eager \
-                        --structured-outputs-config.backend guidance \
                         --dtype float32\
                         --port 8002
