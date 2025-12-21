02df56c3cdb2f4b8992154488a09275a76ff04d4
heliosraz <heliosra3721@gmail.com>
2025-12-21 11:48:29 -0800

    experimenting with max_nums_batched_tokens, max_model_len, and max-num-seqs to maximize through put

--- shell/models/llama.sh
+++ shell/models/llama.sh
@@ -1,5 +1,6 @@
-vllm serve meta-llama/Llama-3.2-3B-Instruct --max_num_batched_tokens 7376 \
-                        --max_model_len 7376 \
+vllm serve meta-llama/Llama-3.2-3B-Instruct  --max_num_batched_tokens 32768 \
+                        --max_model_len 32768 \
+                        --max-num-seqs 256 \
                         --enforce-eager \
                         --dtype float32\
                         --port 8002
