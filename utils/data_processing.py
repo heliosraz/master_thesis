6c92d7959270476bc10119a3a5fb1ebca88e9a0c
heliosraz <heliosra3721@gmail.com>
2025-12-20 23:10:41 -0800

    fixing jsonl loading issues

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -7,11 +7,13 @@ def load_data(task: str, path: str="../data/tasks"):
     return data
 
 def load_system_prompt(id: str, path: str = "data/prompts/system_prompt.jsonl"):
-    data = []
     with open(path, "r") as f:
         for line in f:
-            prompt = json.load(line)
+            prompt = json.loads(line)
             if prompt["id"]==id:
-                return prompt["prompt"]
+                return prompt["SYSTEM_PROMPT"]
     raise KeyError("Desired prompt id doesn't exist.")
+
+if __name__ == "__main__":
+    print(load_system_prompt("1"))
     
