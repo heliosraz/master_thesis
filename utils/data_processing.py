e0ad77265964a5c2da47d1c6d9f197a617d1ec87
heliosraz <heliosra3721@gmail.com>
2025-12-20 23:05:01 -0800

    jsonl has no demlimiter

--- utils/data_processing.py
+++ utils/data_processing.py
@@ -7,12 +7,11 @@ def load_data(task: str, path: str="../data/tasks"):
     return data
 
 def load_system_prompt(id: str, path: str = "data/prompts/system_prompt.jsonl"):
-    with open(path, "r") as fp:
-        data = json.load(fp)
-
-    for inst in data:
-        if inst["id"]==id:
-            return inst
-    else:
-        raise KeyError("Desired prompt id doesn't exist.")
+    data = []
+    with open(path, "r") as f:
+        for line in f:
+            prompt = json.load(line)
+            if prompt["id"]==id:
+                return prompt["prompt"]
+    raise KeyError("Desired prompt id doesn't exist.")
     
